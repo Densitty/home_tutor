d07a68e9dfc77cff6cb736587b527031c529e11f
Densitty <dennis.brough@yandex.com>
2022-01-14 05:14:13 -0600

    creating the component pages and adding importing to router

--- src/router.js
+++ src/router.js
@@ -1,21 +1,28 @@
 import { createRouter, createWebHistroy } from "vue-router";
 
+import TutorDetail from "./pages/tutors/TutorDetail.vue";
+import TutorRegistration from "./pages/tutors/TutorRegistration.vue";
+import Tutors from "./pages/tutors/TutorsList.vue";
+import ContactTutor from "./pages/requests/ContactTutor.vue";
+import RequestReceived from "./pages/requests/RequestsReceived.vue";
+import NotFound from "./pages/NotFound.vue";
+
 const router = createRouter({
   history: createWebHistroy(),
   routes: [
     { path: "/", redirect: "/tutors" },
-    { path: "/tutors", component: null },
+    { path: "/tutors", component: Tutors },
     {
       path: "/tutors/:id",
-      component: null,
+      component: TutorDetail,
       children: {
         path: "contact",
-        component: null,
+        component: ContactTutor,
       } /* for /tutors/:id/contact */,
     },
-    { path: "/register", component: null },
-    { path: "/requests", component: null },
-    { path: "/:notFound(.*)", component: null },
+    { path: "/register", component: TutorRegistration },
+    { path: "/requests", component: RequestReceived },
+    { path: "/:notFound(.*)", component: NotFound },
   ],
 });
 
