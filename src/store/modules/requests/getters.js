b13b32868f86147c948617dd30e7b202752e9b43
Densitty <dennis.brough@yandex.com>
2022-01-18 09:29:52 -0600

    fetching the request messages that have the tutorId matching the userId (i.e. logged-in tutor)

--- src/store/modules/requests/getters.js
+++ src/store/modules/requests/getters.js
@@ -1,10 +1,20 @@
 export default {
-  requests(state) {
-    return state.requests;
+  requests(state, getters, rootState, rootGetters) {
+    // fetch the id of the coach currently logged in
+    const tutorID = rootGetters.userID;
+
+    // loop through the  (local) state to find the request which has the id of the logged in tutor as the tutorID
+    return state.requests.filter((request) => {
+      return request.tutorId === tutorID;
+    });
   },
-  hasRequests(state) {
+  hasRequests(state, getters) {
     // console.log(state);
-    return state.requests && state.requests.length > 0;
+    // return state.requests && state.requests.length > 0;
+
+    // get the request message that has the tutorId property value as that of the userId and not all the request messages in state
+    // console.log(getters.requests);
+    return getters.requests && getters.requests.length > 0;
   },
   /* isTutor(state, getters, rootState, rootGetters) {
     // get all tutors from the  tutors module
