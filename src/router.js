45d2f77d44ba61e3b469a4df6d7ce86e607cc0fa
Densitty <dennis.brough@yandex.com>
2022-01-25 23:00:00 -0600

    adding navigation guard to some routes

--- src/router.js
+++ src/router.js
@@ -8,6 +8,8 @@ import RequestReceived from "./pages/requests/RequestsReceived.vue";
 import UserAuth from "./pages/auth/UserAuth.vue";
 import NotFound from "./pages/NotFound.vue";
 
+import store from "./store/index";
+
 const router = createRouter({
   history: createWebHistory(),
   routes: [
@@ -24,11 +26,29 @@ const router = createRouter({
         },
       ],
     },
-    { path: "/register", component: TutorRegistration },
-    { path: "/requests", component: RequestReceived },
-    { path: "/auth", component: UserAuth },
+    {
+      path: "/register",
+      component: TutorRegistration,
+      meta: { requiresAuth: true },
+    },
+    {
+      path: "/requests",
+      component: RequestReceived,
+      meta: { requiresAuth: true },
+    },
+    { path: "/auth", component: UserAuth, meta: { requiresNoAuth: true } },
     { path: "/:notFound(.*)", component: NotFound },
   ],
+});
+
+router.beforeEach(function (to, from, next) {
+  if (to.meta.requiresAuth && !store.getters.isAuthenticated) {
+    next("/auth");
+  } else if (to.meta.requiresNoAuth && store.getters.isAuthenticated) {
+    next("/");
+  } else {
+    next();
+  }
 });
 
 export default router;
